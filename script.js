ea6b9c404f3d3b1fe8cc981ae3af23747b4abdcc
hichoudhary22 <hichoudhary22@gmail.com>
2023-09-26 17:07:01 +0530

    vibration tewaking 2

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function checkFunction(userInput) {
     console.log("try again");
     wrongBoard.textContent = `wrong ${++wrong}`;
     inputAnswer.focus();
-    navigator.vibrate(100);
+    navigator.vibrate(200);
     boardColor(false);
   }
 }
