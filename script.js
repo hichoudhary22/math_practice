a5e0b164473f000ac2bf166f6a2100f4fa92f538
hichoudhary22 <hichoudhary22@gmail.com>
2023-10-03 22:22:26 +0530

    working marc3 code

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ const modalWindow = document.querySelector(".modal-window");
 const modalWindowContent = document.querySelector(".modal-window-content");
 
 //general modal window elements----------------------->
-const btnSaveLimitsData = document.querySelector(".save-limits-data");
 const btnCancel = document.querySelector(".hide-modal-window");
 const inputLowerFirst = document.querySelector(".lower-first");
 const inputUpperFirst = document.querySelector(".upper-first");
@@ -48,10 +47,13 @@ const modalWindowContentMultiplicationData = document.querySelector(
 const modalWindowContentDivideData = document.querySelector(
   ".modal-window-content-divideData"
 );
+const modalWindowContentMoreData = document.querySelector(
+  ".modal-window-content-more"
+);
 
 // specific elements of specific modal windows---------------------------->
-const continiousAdditionProblems = document.querySelector(
-  ".continious-addition-problems"
+const continiousAdditionProblemsCheckbox = document.querySelector(
+  ".continious-addition-problems-checkbox"
 );
 const learnTableMultiplicationCheckbox = document.querySelector(
   ".learn-table-multiplication-checkbox"
@@ -66,7 +68,22 @@ const learnTableDivideNumber = document.querySelector(
   ".learn-table-divide-number"
 );
 
-// variables to use in app-------------------->
+const mixedCalculationCheckbox = document.querySelector(
+  ".mixed-calculation-checkbox"
+);
+
+// specific save buttons-------------------------------->
+const btnAdditionSaveData = document.querySelector(".btn-addition-save-data");
+const btnSubtractionSaveData = document.querySelector(
+  ".btn-subtraction-save-data"
+);
+const btnMultiplicationSaveData = document.querySelector(
+  ".btn-multiplication-save-data"
+);
+const btnDivideSaveData = document.querySelector(".btn-divide-save-data");
+const btnMoreSaveData = document.querySelector(".btn-more-save-data");
+
+// global variables to use in app-------------------->
 let appRangeData = {
   lowerFirst: 10,
   upperFirst: 20,
@@ -82,7 +99,8 @@ let x,
   timerSeconds,
   dataType = "additionData",
   result,
-  continious_addition_problems = false;
+  continious_addition_problems = false,
+  mixedCalculation = false;
 
 // code starts here---------------->
 
@@ -100,9 +118,9 @@ function readData() {
 
 function displayRandomNumbers() {
   x =
-    !continious_addition_problems && operator === "+"
-      ? randomInt(appRangeData.lowerFirst, appRangeData.upperFirst)
-      : result;
+    continious_addition_problems && operator === "+"
+      ? result
+      : randomInt(appRangeData.lowerFirst, appRangeData.upperFirst);
   y = randomInt(appRangeData.lowerSecond, appRangeData.upperSecond);
 
   operator === "-" && x < y ? ([x, y] = [y, x]) : null;
@@ -112,7 +130,7 @@ function displayRandomNumbers() {
   inputSecondNumber.value = y;
 
   inputAnswer.focus();
-  resultFunction();
+  calculateAnswer();
 }
 
 function randomInt(lowerLimit, upperLimit) {
@@ -121,7 +139,7 @@ function randomInt(lowerLimit, upperLimit) {
   return number;
 }
 
-function resultFunction() {
+function calculateAnswer() {
   switch (operator) {
     case "+":
       result = x + y;
@@ -136,23 +154,23 @@ function resultFunction() {
       result = x / y;
       break;
     default:
-      console.log("error in operator division of resultFunction");
+      console.log("error in operator division of calculateAnswer");
   }
 }
 
-function checkFunction(userInput) {
+function checkUserInput(userInput) {
   if (userInput === result) {
     inputAnswer.value = "";
     right_or_wrong.textContent = "right!!!!!";
     output.textContent = `${x} ${operator} ${y} = ${result}`;
-    displayRandomNumbers();
+    mixedCalculation ? randomOperatorGenerator() : displayRandomNumbers();
   } else {
     output.textContent = "";
     right_or_wrong.textContent = "not there yet!!! keep trying...";
   }
 }
 
-function operatorChange() {
+function modifyDataType() {
   switch (operator) {
     case "+":
       dataType = "additionData";
@@ -171,7 +189,7 @@ function operatorChange() {
   }
 }
 
-function randomOperator() {
+function randomOperatorGenerator() {
   switch (Math.floor(Math.random() * 4)) {
     case 0:
       operator = "+";
@@ -189,7 +207,8 @@ function randomOperator() {
       console.log("problem in random operator");
   }
   selectOperator.value = operator;
-  operatorChange();
+  modifyDataType();
+  readData();
   displayRandomNumbers();
 }
 
@@ -201,11 +220,12 @@ function displayData() {
 }
 
 function bottomNavBarButtonPressed(newOperator) {
-  inputFirstNumber.value = inputSecondNumber.value = "";
   hideAllModalWindows();
+  resetCheckboxesAndAppBooleans();
+  inputFirstNumber.value = inputSecondNumber.value = "";
   inputAnswer.disabled = true;
   operator = newOperator;
-  operatorChange();
+  modifyDataType();
   readData();
   displayData();
   modalWindow.style.display = "flex";
@@ -220,14 +240,116 @@ function hideAllModalWindows() {
   modalWindowContentSubtractionData.style.display = "none";
   modalWindowContentMultiplicationData.style.display = "none";
   modalWindowContentDivideData.style.display = "none";
-  continiousAdditionProblems.checked = continious_addition_problems;
-  inputAnswer.disabled = false;
+  modalWindowContentMoreData.style.display = "none";
 }
-function learnTable(number) {
-  if (number) {
-    appRangeData.lowerFirst = appRangeData.upperFirst = number;
+
+function saveAppRangeDataToLocalStorage() {
+  // reading data from DOM and modifying the appRangeData-------->
+  appRangeData.lowerFirst = Number(inputLowerFirst.value);
+  appRangeData.upperFirst = Number(inputUpperFirst.value);
+  appRangeData.lowerSecond = Number(inputLowerSecond.value);
+  appRangeData.upperSecond = Number(inputUpperSecond.value);
+
+  //clearing local storage------------->
+  localStorage.removeItem(dataType);
+
+  //setting new ranges in the local storage------------->
+  localStorage.setItem(dataType, JSON.stringify(appRangeData));
+}
+
+function cancelButtonTasks(passedOperator) {
+  hideAllModalWindows();
+  inputAnswer.disabled = false;
+  passedOperator
+    ? (selectOperator.value = operator = passedOperator)
+    : randomOperatorGenerator();
+  modifyDataType();
+  displayRandomNumbers();
+}
+function resetCheckboxesAndAppBooleans() {
+  continiousAdditionProblemsCheckbox.checked =
+    mixedCalculationCheckbox.checked =
+    continious_addition_problems =
+    mixedCalculation =
+    learnTableMultiplicationCheckbox.checked =
+    learnTableDivideCheckbox.checked =
+      false;
+}
+
+// giving javascript power to modify contents of the page---------------------------->
+
+inputAnswer.addEventListener("keyup", () =>
+  checkUserInput(Number(inputAnswer.value))
+);
+
+selectOperator.addEventListener("change", () => {
+  modifyDataType();
+  displayRandomNumbers();
+});
+
+// adding event listeners to bottom nav bar buttons--------------------------------->
+btnAddition.addEventListener("click", () => bottomNavBarButtonPressed("+"));
+btnSubtraction.addEventListener("click", () => bottomNavBarButtonPressed("-"));
+btnMultiplication.addEventListener("click", () =>
+  bottomNavBarButtonPressed("*")
+);
+btnDivide.addEventListener("click", () => bottomNavBarButtonPressed("/"));
+btnMore.addEventListener("click", () => {
+  hideAllModalWindows();
+  modalWindow.style.display = "flex";
+  modalWindowContent.style.display = "block";
+  modalWindowContentMoreData.style.display = "block";
+  mixedCalculationCheckbox.checked = false;
+  inputFirstNumber.value = inputSecondNumber.value = "";
+  inputAnswer.disabled = true;
+});
+
+window.onclick = function (event) {
+  if (event.target == modalWindow) cancelButtonTasks(selectOperator.value);
+};
+
+btnCancel.addEventListener("click", () => {
+  cancelButtonTasks(selectOperator.value);
+});
+
+btnAdditionSaveData.addEventListener("click", () => {
+  continious_addition_problems
+    ? (result = 0)
+    : saveAppRangeDataToLocalStorage();
+  cancelButtonTasks("+");
+});
+
+continiousAdditionProblemsCheckbox.addEventListener("change", function () {
+  continious_addition_problems = this.checked;
+  if (this.checked) {
+    inputLowerFirst.value = inputUpperFirst.value = "";
+    inputLowerFirst.disabled = inputUpperFirst.disabled = true;
+  } else {
+    displayData();
+    inputLowerFirst.disabled = inputUpperFirst.disabled = false;
+  }
+});
+
+btnSubtractionSaveData.addEventListener("click", () => {
+  saveAppRangeDataToLocalStorage();
+  cancelButtonTasks("-");
+});
+
+btnMultiplicationSaveData.addEventListener("click", () => {
+  if (learnTableMultiplicationCheckbox.checked) {
+    appRangeData.lowerFirst = appRangeData.upperFirst = Number(
+      learnTableMultiplicationNumber.value
+    );
     appRangeData.lowerSecond = 2;
     appRangeData.upperSecond = 9;
+  } else {
+    saveAppRangeDataToLocalStorage();
+  }
+  cancelButtonTasks("*");
+});
+
+learnTableMultiplicationCheckbox.addEventListener("change", function () {
+  if (this.checked) {
     inputLowerFirst.disabled =
       inputUpperFirst.disabled =
       inputLowerSecond.disabled =
@@ -238,6 +360,8 @@ function learnTable(number) {
       inputLowerSecond.value =
       inputUpperSecond.value =
         "";
+    learnTableMultiplicationNumber.disabled = false;
+    learnTableMultiplicationNumber.focus();
   } else {
     inputLowerFirst.disabled =
       inputUpperFirst.disabled =
@@ -245,85 +369,54 @@ function learnTable(number) {
       inputUpperSecond.disabled =
         false;
     displayData();
+    learnTableMultiplicationNumber.disabled = true;
+    learnTableMultiplicationNumber.value = "";
   }
-}
-
-// giving javascript power to modify contents of the page---------------------------->
-
-btnSaveLimitsData.addEventListener("click", () => {
-  /* 
-    1. get the numbers from DOM
-    2. modify the numbers range object
-    3. generate random numbers and display them and update first number and second number accordingly
-    4. clear local storate of old data
-    5. set new data
-     */
-  // reading data from DOM and modifying the appRangeData-------->
-
-  appRangeData.lowerFirst = Number(inputLowerFirst.value);
-  appRangeData.upperFirst = Number(inputUpperFirst.value);
-  appRangeData.lowerSecond = Number(inputLowerSecond.value);
-  appRangeData.upperSecond = Number(inputUpperSecond.value);
-
-  //displaying random numbers according to the new range------------->
-  //this signify that the range has been updated
-
-  //clearing local storage------------->
-  localStorage.removeItem(dataType);
-
-  //setting new ranges in the local storage------------->
-  localStorage.setItem(dataType, JSON.stringify(appRangeData));
-
-  // closing the modal window-------------------->
-  readData();
-
-  right_or_wrong.textContent = "data saved";
-  if (continiousAdditionProblems.checked) {
-    continious_addition_problems = continiousAdditionProblems.checked;
-    result = 0;
-  }
-  operatorChange();
-  displayRandomNumbers();
-  hideAllModalWindows();
 });
 
-inputAnswer.addEventListener("keyup", () =>
-  checkFunction(Number(inputAnswer.value))
-);
-
-selectOperator.addEventListener("change", () => {
-  operatorChange();
-  displayRandomNumbers();
-});
-
-btnAddition.addEventListener("click", () => bottomNavBarButtonPressed("+"));
-btnSubtraction.addEventListener("click", () => bottomNavBarButtonPressed("-"));
-btnMultiplication.addEventListener("click", () =>
-  bottomNavBarButtonPressed("*")
-);
-btnDivide.addEventListener("click", () => bottomNavBarButtonPressed("/"));
-
-window.onclick = function (event) {
-  if (event.target == modalWindow) {
-    modalWindow.style.display = "none";
+btnDivideSaveData.addEventListener("click", () => {
+  if (learnTableDivideCheckbox.checked) {
+    appRangeData.lowerSecond = appRangeData.upperSecond = Number(
+      learnTableDivideNumber.value
+    );
+    appRangeData.lowerFirst = 2;
+    appRangeData.upperFirst = 9;
+  } else {
+    saveAppRangeDataToLocalStorage();
   }
-};
-btnCancel.addEventListener("click", () => {
-  operator = selectOperator.value;
-  operatorChange();
-  displayRandomNumbers();
-  hideAllModalWindows();
+  cancelButtonTasks("/");
 });
 
 learnTableDivideCheckbox.addEventListener("change", function () {
-  learnTable(this.checked ? Number(learnTableDivideNumber.value) : null);
-});
-learnTableMultiplicationCheckbox.addEventListener("change", function () {
-  learnTable(
-    this.checked ? Number(learnTableMultiplicationNumber.value) : null
-  );
+  if (this.checked) {
+    inputLowerFirst.disabled =
+      inputUpperFirst.disabled =
+      inputLowerSecond.disabled =
+      inputUpperSecond.disabled =
+        true;
+    inputLowerFirst.value =
+      inputUpperFirst.value =
+      inputLowerSecond.value =
+      inputUpperSecond.value =
+        "";
+    learnTableDivideNumber.disabled = false;
+    learnTableDivideNumber.focus();
+  } else {
+    inputLowerFirst.disabled =
+      inputUpperFirst.disabled =
+      inputLowerSecond.disabled =
+      inputUpperSecond.disabled =
+        false;
+    displayData();
+    learnTableDivideNumber.disabled = true;
+    learnTableDivideNumber.value = "";
+  }
 });
 
+btnMoreSaveData.addEventListener("click", () => {
+  mixedCalculation = mixedCalculationCheckbox.checked ? true : false;
+  cancelButtonTasks();
+});
 /* 
 step 01: check if limits are already inserted in the web storage if available update variable
 step 02: if not use default limits from js script and set it to DOM
